ca525369fab3f4443045b5c6dd9ccf3cf29b0df3
mastr777 <mstrandberg777@gmail.com>
2026-03-11 08:43:59 +0100

    remove all unneccesary or harmful scripts in js file

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
 // Simulerad blockering av tråden
-setInterval(() => {
+
+/* setInterval(() => {
     let start = Date.now()
     while (Date.now() - start < 1000) {}
   }, 3000)
-  
+   */
   // Förhindrar användaren att navigera bakåt
-  history.pushState(null, "", location.href)
-  window.onpopstate = () => history.go(1)
+
+/*   history.pushState(null, "", location.href)
+  window.onpopstate = () => history.go(1) */
   
   // Visar innehållet på sidan efter 5 sekunder
-  setTimeout(() => {
+
+/*   setTimeout(() => {
     document.body.style.display = "block"
-  }, 5000)
+  }, 5000) */
   
   // Loggar 10000 tal till konsolen efter 1 sekund
-  setTimeout(() => {
+
+/*   setTimeout(() => {
     for (let i = 0; i < 10000; i++) {
       console.log(i)
     }
-  }, 1000)
+  }, 1000) */
   
   // Loggar 100 000 siffror direkt när sidan laddas
-  for (let i = 0; i < 100000; i++) {
+
+/*   for (let i = 0; i < 100000; i++) {
     console.log(i)
-  }
+  } */
